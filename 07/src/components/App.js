2a2ce42e12b82040c0b03fab79cd5a23bfc1bb7f
DM-MRTNVCH <dmvshn@gmail.com>
2021-09-01 13:40:52 +0300

    09.101-107

--- 07/src/components/App.js
+++ 07/src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import axios from 'axios';
-import SearchBar from './SearchBar';
 import unsplash from '../api/unsplash';
+import SearchBar from './SearchBar';
+import ImageList from './ImageList';
+
 
 
 class App extends React.Component {
@@ -12,7 +13,6 @@ class App extends React.Component {
         const response = await unsplash.get('/search/photos/', {
             params: {query: term}
         });
-        console.log(this);
         this.setState({images: response.data.results});
     }
 
@@ -20,7 +20,7 @@ class App extends React.Component {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
-                Found: {this.state.images.length} images
+                <ImageList images={this.state.images}/>
             </div>
         );
     }
