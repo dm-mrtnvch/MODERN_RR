95b0e03e1633f9919041e1de1d4bf4513c3bd90f
DM-MRTNVCH <dmvshn@gmail.com>
2021-09-03 23:49:33 +0300

    12.159-169

--- 12/src/components/Search.js
+++ 12/src/components/Search.js
@@ -4,14 +4,14 @@ import axios from 'axios';
 const Search = () => {
 
     const [term, setTerm] = useState('programming');
-    const [results, setResults] = useState([])
+    const [results, setResults] = useState([]);
 
 
     console.log(results);
 
     useEffect(() => {
         const search = async () => {
-           const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
@@ -19,13 +19,50 @@ const Search = () => {
                     format: 'json',
                     srsearch: term
                 }
-            })
-            setResults(data.query.search)
+            });
+            setResults(data.query.search);
 
         };
-        search()
-    }, [term]);
 
+        if(term && !results.length) {
+            search()
+        } else {
+            const timeoutId = setTimeout(() => {
+                if (term) {
+                    search();
+                }
+            }, 500);
+
+            return () => {
+                clearTimeout(timeoutId)
+            }
+        }
+    }, [term, results.length]);
+
+
+
+
+    const renderResults = results.map(result => {
+        return (
+            <div className="item" key={result.pageid}>
+                <div className="right floated content">
+                    <a
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+                    {/*{result.snippet}*/}
+                </div>
+            </div>
+        );
+    });
 
     return (
         <div>
@@ -38,6 +75,9 @@ const Search = () => {
                         onChange={e => setTerm(e.target.value)}
                     />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderResults}
             </div>
         </div>
     );
