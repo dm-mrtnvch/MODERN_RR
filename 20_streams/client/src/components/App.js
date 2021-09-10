d7dec2b3f96437dcd943be42146fb0231cca57d5
DM-MRTNVCH <dmvshn@gmail.com>
2021-09-10 18:06:23 +0300

    20.up to 297

---
+++ 20_streams/client/src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Route, BrowserRouter, Link} from 'react-router-dom';
+
+
+
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default App;
+
---
+++ 20_streams/client/src/components/streams/StreamDelete.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StreamDetele = () => {
+    return (
+        <div>
+            StreamDetele
+        </div>
+    );
+};
+
+export default StreamDetele;
