f929b3d97452c33685bb92d71a9946fd9745b938
DM-MRTNVCH <dmvshn@gmail.com>
2021-09-11 16:48:47 +0300

    20.to the end

--- 20_streams/client/src/components/App.js
+++ 20_streams/client/src/components/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import {Route, BrowserRouter, Link} from 'react-router-dom';
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamDetele from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 
 
 
 const App = () => {
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
                 <div>
+                    <Header/>
+                    <Route path="/" exact component={StreamList}/>
+                    <Route path="/streams/new" exact component={StreamCreate}/>
+                    <Route path="/streams/edit" exact component={StreamEdit}/>
+                    <Route path="/streams/delete" exact component={StreamDetele}/>
+                    <Route path="/streams/show" exact component={StreamShow}/>
                 </div>
             </BrowserRouter>
         </div>
     );
 };
+
+
+
+
 
 export default App;
 
